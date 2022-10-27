2f64cbe000fd59fbbac6edfe693997392d9232bc
Clint Shannon <clintshannon13@gmail.com>
2022-10-26 17:29:55 -0700

    added pos and neg analysis

--- rsa-pycharm/get_and_clean_data.py
+++ rsa-pycharm/get_and_clean_data.py
@@ -9,6 +9,7 @@ import seaborn as sns
 import praw
 
 # Create a reddit client
+# Get info here https://www.reddit.com/prefs/apps/
 reddit = praw.Reddit(client_id='TtaGbxrcEqrFPqjHoFyZig',
                      client_secret='ANwuNmen1-zv_7rZ-JJp7n7nx3bIGg',
                      user_agent='sentiment-analysis')
@@ -39,7 +40,49 @@ for line in headlines:
 df = pd.DataFrame.from_records(results)
 df.head(5)
 
+# Assign Pos and Neg Values
 df['label'] = 0
 df.loc[df['compound'] > 0.2, 'label'] = 1
 df.loc[df['compound'] < -0.2, 'label'] = -1
 print(df.loc[df['label'] != 0].head(5))
+
+# Positive and Negative Stats
+print(df.label.value_counts())
+print(df.label.value_counts(normalize=True) * 100)
+
+# Plot Pos and Neg Stats
+fig, ax = plt.subplots(figsize=(8, 8))
+counts = df.label.value_counts(normalize=True) * 100
+sns.barplot(x=counts.index, y=counts, ax=ax)
+ax.set_xticklabels(['Negative', 'Neutral', 'Positive'])
+ax.set_ylabel("Percentage")
+
+# Processing Text
+from nltk.tokenize import RegexpTokenizer
+tokenizer = RegexpTokenizer(r'\w+')
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+
+# Define a function to tokenize headlines and remove stopwords.
+
+
+def process_text(headlines):
+    tokens = []
+    for line in headlines:
+        toks = tokenizer.tokenize(line)
+        toks = [t.lower() for t in toks if t.lower() not in stop_words]
+        tokens.extend(toks)
+    return tokens
+
+
+# Positive words
+pos_lines = list(df[df.label == 1].headline)
+pos_tokens = process_text(pos_lines)
+pos_freq = nltk.FreqDist(pos_tokens)
+print(pos_freq.most_common(10))
+
+# Negative words
+neg_lines = list(df[df.label == -1].headline)
+neg_tokens = process_text(neg_lines)
+neg_freq = nltk.FreqDist(neg_tokens)
+print(neg_freq.most_common(10))
